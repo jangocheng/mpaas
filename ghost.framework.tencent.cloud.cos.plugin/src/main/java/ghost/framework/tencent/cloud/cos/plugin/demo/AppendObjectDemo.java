21f948c270a1e5674f64b4d7031d86dcd6d4c612
13715848993 <27048384@qq.com>
2020-09-06 01:12:20 +0800

    修改

--- ghost.framework.tencent.cloud.cos.plugin/src/main/java/ghost/framework/tencent/cloud/cos/plugin/demo/AppendObjectDemo.java
+++ ghost.framework.tencent.cloud.cos.plugin/src/main/java/ghost/framework/tencent/cloud/cos/plugin/demo/AppendObjectDemo.java
@@ -15,9 +15,9 @@ import java.io.File;
 public class AppendObjectDemo {
     public static void AppendObjectFromLocal() {
         // 1 初始化用户身份信息(secretId, secretKey)
-        COSCredentials cred = new BasicCOSCredentials("AKIDXXXXXXXX", "1A2Z3YYYYYYYYYY");
+        COSCredentials cred = new BasicCOSCredentials("", "");
         // 2 设置bucket的区域, COS地域的简称请参照 https://www.qcloud.com/document/product/436/6224
-        ClientConfig clientConfig = new ClientConfig(new Region("ap-guangzhou"));
+        ClientConfig clientConfig = new ClientConfig(new Region(""));
         // 3 生成cos客户端
         COSClient cosclient = new COSClient(cred, clientConfig);
         // bucket名需包含appid
